e1284cef38f8ac81941eac60039605c70305e15e
marcopgordillo <marcopgordillo@gmail.com>
2018-11-26 17:25:47 -0500

    first commit

---
+++ src/main/java/com/example/springbootbackendapirest/SpringbootBackendApirestApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootbackendapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApirestApplication.class, args);
+	}
+}
