7bdef80bc7fedbd2f41d9744f280131bdf231536
marcopgordillo <marcopgordillo@gmail.com>
2018-11-27 15:50:40 -0500

    Manejando errores en el backend

--- src/main/java/com/example/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/example/springbootbackendapirest/controllers/ClienteRestController.java
@@ -2,10 +2,14 @@ package com.example.springbootbackendapirest.controllers;
 
 import com.example.springbootbackendapirest.models.entity.Cliente;
 import com.example.springbootbackendapirest.models.services.IClienteService;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins={"http://localhost:4200"})
 @RestController
@@ -24,8 +28,26 @@ public class ClienteRestController {
     }
 
     @GetMapping("/clientes/{id}")
-    public Cliente show(@PathVariable Long id) {
-        return this.clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Cliente cliente;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            cliente = this.clienteService.findById(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos!");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (cliente == null) {
+            response.put("mensaje", "El cliente ID: ".concat(id.toString()).concat(" no existe en la base de datos!"));
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+        // return this.clienteService.findById(id);
     }
 
     @PostMapping("/clientes")
