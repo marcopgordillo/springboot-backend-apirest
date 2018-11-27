1587e53c27809c58280caa39826cd16dfe1d468a
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-26 23:04:48 -0500

    CORS added

--- src/main/java/com/example/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/example/springbootbackendapirest/controllers/ClienteRestController.java
@@ -2,12 +2,14 @@ package com.example.springbootbackendapirest.controllers;
 
 import com.example.springbootbackendapirest.models.entity.Cliente;
 import com.example.springbootbackendapirest.models.services.IClienteService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins={"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
