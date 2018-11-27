1582ee7ba401b4a0a0ab1c4457bf73ff15c50411
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-26 22:19:11 -0500

    Creando la entity Cliente

---
+++ src/main/java/com/example/springbootbackendapirest/models/entity/Cliente.java
@@ -0,0 +1,94 @@
+package com.example.springbootbackendapirest.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(getId(), cliente.getId()) &&
+                Objects.equals(getNombre(), cliente.getNombre()) &&
+                Objects.equals(getApellido(), cliente.getApellido()) &&
+                Objects.equals(getEmail(), cliente.getEmail()) &&
+                Objects.equals(getCreateAt(), cliente.getCreateAt());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId(), getNombre(), getApellido(), getEmail(), getCreateAt());
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", createAt=" + createAt +
+                '}';
+    }
+}
