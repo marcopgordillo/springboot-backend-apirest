204b8df1d30be9bcb38c47f4153f3544d54512c4
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-30 00:19:00 -0500

    Probando la autenticación con Postman y obteniento el token JWT.

--- src/main/java/com/example/springbootbackendapirest/SpringbootBackendApirestApplication.java
+++ src/main/java/com/example/springbootbackendapirest/SpringbootBackendApirestApplication.java
@@ -1,14 +1,33 @@
 package com.example.springbootbackendapirest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableCaching
-public class SpringbootBackendApirestApplication {
+public class SpringbootBackendApirestApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+  private final BCryptPasswordEncoder passwordEncoder;
+
+  public SpringbootBackendApirestApplication(BCryptPasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApirestApplication.class, args);
 	}
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    String password = "12345";
+
+    for (int i = 0; i < 4; i++) {
+      String passwordBcrypt = passwordEncoder.encode(password);
+      System.out.println(passwordBcrypt);
+    }
+  }
 }
