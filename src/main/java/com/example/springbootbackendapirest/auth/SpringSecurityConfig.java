47f23c32052badfe95890eec9bc9f89cdb6885db
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-29 23:55:09 -0500

    Añadiendo la configuración para el servidor de recurso.

--- src/main/java/com/example/springbootbackendapirest/auth/SpringSecurityConfig.java
+++ src/main/java/com/example/springbootbackendapirest/auth/SpringSecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -30,8 +32,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Override
-  @Autowired
+  @Autowired // Para que AuthenticationManagerBuilder pueda ser inyectado por método.
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+  }
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 }
