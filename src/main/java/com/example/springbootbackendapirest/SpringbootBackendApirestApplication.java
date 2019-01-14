8c670f8814b5ab3b0f5603589222900bbe43dcd4
marcopgordillo <marcopgordillo@gmail.com>
2019-01-14 17:32:14 -0500

    Añadido análisis y diseñó OO con UML Diagrama de Clases de Dominio

--- src/main/java/com/example/springbootbackendapirest/SpringbootBackendApirestApplication.java
+++ src/main/java/com/example/springbootbackendapirest/SpringbootBackendApirestApplication.java
@@ -10,24 +10,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableCaching
 public class SpringbootBackendApirestApplication implements CommandLineRunner {
 
-  private final BCryptPasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-  public SpringbootBackendApirestApplication(BCryptPasswordEncoder passwordEncoder) {
-    this.passwordEncoder = passwordEncoder;
-  }
-
-  public static void main(String[] args) {
-		SpringApplication.run(SpringbootBackendApirestApplication.class, args);
-	}
-
-  @Override
-  public void run(String... args) throws Exception {
-
-    String password = "12345";
-
-    for (int i = 0; i < 4; i++) {
-      String passwordBcrypt = passwordEncoder.encode(password);
-      System.out.println(passwordBcrypt);
+    public SpringbootBackendApirestApplication(BCryptPasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
-  }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApirestApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        String password = "12345";
+
+        for (int i = 0; i < 4; i++) {
+            String passwordBcrypt = passwordEncoder.encode(password);
+            System.out.println(passwordBcrypt);
+        }
+    }
 }
