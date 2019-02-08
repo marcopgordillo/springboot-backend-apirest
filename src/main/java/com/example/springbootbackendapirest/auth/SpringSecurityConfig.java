1f950686bc9d1373a829ba93f233e91c60848082
marcopgordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-02-08 10:08:55 -0500

    Añadido Qualifier para definir la implementacion custom, y añadido ** a cors

--- src/main/java/com/example/springbootbackendapirest/auth/SpringSecurityConfig.java
+++ src/main/java/com/example/springbootbackendapirest/auth/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springbootbackendapirest.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,7 +19,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserDetailsService usuarioService;
 
-  public SpringSecurityConfig(UserDetailsService usuarioService) {
+  public SpringSecurityConfig(@Qualifier("usuarioServiceImpl") UserDetailsService usuarioService) {
     this.usuarioService = usuarioService;
   }
 
