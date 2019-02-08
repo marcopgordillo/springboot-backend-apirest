edd2268e77c64982667ee3e2b4e41276ec6ed889
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-02-08 02:01:02 -0500

    actualizado CORS con la direccion del frontend

--- src/main/java/com/example/springbootbackendapirest/controllers/FacturaRestController.java
+++ src/main/java/com/example/springbootbackendapirest/controllers/FacturaRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:4200", "*"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://clientes-app-angular-c559f.firebaseapp.com"})
 @RestController
 @RequestMapping("/api")
 public class FacturaRestController {
